9f38a15decedb4ee16bad1a90218b705a9275dec
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2023-11-04 19:02:14 +0300

    Added tha second part( first round

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,7 +1,19 @@
-function playRound(playerSelection, computerSelection) {
-  // your code here!
-}
+function getComputerChoice() {
+  let number = Math.floor(Math.random() * 3);
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+  if (number === 0) {
+    return "rock";
+  }
+  if (number === 1) {
+    return "paper";
+  }
+  if (number === 2) {
+    return "scissor";
+  }
+}
+function play(playerSelection, getComputerChoice) {
+  answer = prompt("rock paper scissor?");
+  playerSelection = answer.toLowerCase();
+
+  console.log(playerSelection);
+}
