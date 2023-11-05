03c628b0168f461f095881264d8967176f96d4ed
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2023-11-05 08:19:34 +0300

    finished the first round. next is the loop

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,19 +1,54 @@
-function getComputerChoice() {
-  let number = Math.floor(Math.random() * 3);
+let playerSelection, computerSelection;
 
-  if (number === 0) {
-    return "rock";
+function playRound(playerSelection, computerSelection) {
+  function getComputerChoice() {
+    let number = Math.floor(Math.random() * 3);
+
+    if (number === 0) {
+      return "rock";
+    }
+    if (number === 1) {
+      return "paper";
+    }
+    if (number === 2) {
+      return "scissor";
+    }
   }
-  if (number === 1) {
-    return "paper";
+  computerSelection = getComputerChoice();
+  let answer = window.prompt("Rock, paper, scissors?").toLowerCase();
+  playerSelection = answer;
+  if (playerSelection === "rock" && computerSelection === "paper") {
+    return "You Lose! Paper beats Rock";
   }
-  if (number === 2) {
-    return "scissor";
+  if (playerSelection === "rock" && computerSelection === "scissor") {
+    return "You won! rock beats scissor";
+  }
+  if (playerSelection === "rock" && computerSelection === "rock") {
+    return "It's a draw!";
+  }
+  if (playerSelection === "paper" && computerSelection === "paper") {
+    return "It's a draw!";
+  }
+  if (playerSelection === "paper" && computerSelection === "scissor") {
+    return "You lose! scissor beats paper";
+  }
+  if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You won! paper beats rock";
+  }
+  if (playerSelection === "scissor" && computerSelection === "paper") {
+    return "You win! scissor beats paper";
+  }
+  if (playerSelection === "scissor" && computerSelection === "scissor") {
+    return "It's a draw!";
+  }
+  if (playerSelection === "scissor" && computerSelection === "rock") {
+    return "You lose! paper beats rock";
   }
 }
-function play(playerSelection, getComputerChoice) {
-  answer = prompt("rock paper scissor?");
-  playerSelection = answer.toLowerCase();
+console.log(playRound(playerSelection, computerSelection));
 
-  console.log(playerSelection);
-}
+console.log(playRound(playerSelection, computerSelection));
+console.log(playRound(playerSelection, computerSelection));
+
+console.log(playRound(playerSelection, computerSelection));
+console.log(playRound(playerSelection, computerSelection));
